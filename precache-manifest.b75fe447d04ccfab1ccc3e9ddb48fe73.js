c0d742c620268f2c51d1d62f149676d903a31b62
Nicolas GL <nico17.ngl@gmail.com>
2022-01-12 01:16:12 -0600

    Updates

---
+++ precache-manifest.b75fe447d04ccfab1ccc3e9ddb48fe73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c1cea4cf29c63124052908ca4f671f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce7c219c180d36d36e8",
+    "url": "/static/css/main.93a72abb.chunk.css"
+  },
+  {
+    "revision": "fea8f09a83b25709bc17",
+    "url": "/static/js/2.bf8ca7ea.chunk.js"
+  },
+  {
+    "revision": "9feb284d92501b45ec6e6f4f0fb649b3",
+    "url": "/static/js/2.bf8ca7ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ce7c219c180d36d36e8",
+    "url": "/static/js/main.cac06816.chunk.js"
+  },
+  {
+    "revision": "4ffb1d70eaf0cd593f25",
+    "url": "/static/js/runtime-main.58800f15.js"
+  }
+]);
