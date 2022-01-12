c8058947b55f6a69dca4e6d60f0e5d75a0fa83e9
Nicolas GL <nico17.ngl@gmail.com>
2022-01-12 01:33:01 -0600

    Finished

--- src/components/BirthdayMessage.js
+++ src/components/BirthdayMessage.js
@@ -1,7 +1,15 @@
 
 import React from 'react'
 import styled from 'styled-components'
+import profile from './profile.jpg';
 
+const ProfileImage = styled.div`
+  text-align: center;
+  margin: 0;
+  position: relative;
+  top: 0%;
+  left: 2%;
+`
 const Message = styled.div`
   white-space: nowrap;
   font-family: 'Roboto', sans-serif;
@@ -39,7 +47,9 @@ class BirthdayMessage extends React.Component {
   render() {
     return (
       <Container>
-        
+        <ProfileImage>
+          <img src={profile} alt="pic" style={{ width: "15rem" }}/>;
+        </ProfileImage>
         <Message style={{ color: this.props.birthdayData.textColor }}>
           Feliz cumple, {this.props.birthdayData.wishee}!
         </Message>
